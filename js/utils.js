3487783f44b5af8d958b878fda40ac472687be7b
Justin Martin <frozenfire@thefrozenfire.com>
2022-01-04 18:37:24 -0800

    Fix payload-end masking.

--- js/utils.js
+++ js/utils.js
@@ -26,7 +26,7 @@ function getBase64JSONSlice(input, field) {
     const start = Math.floor(lead.length / 4) * 4;
     const end = Math.ceil(((lead.length + target.length) - 1) / 4) * 4;
     
-    return [start, end > (input.length - 1) ? input.length - 1 : end];
+    return [start, end >= input.length ? input.length : end];
 }
 
 function buffer2BitArray(b) {
